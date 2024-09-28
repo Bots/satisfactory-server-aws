59f9e2d47430ea8ecef125dbea2c1610cceaa25b
bots <botsone@gmail.com>
2024-09-27 23:55:42 -0600

    [fix](server-hosting): Refactored lambda function.

--- server-hosting/lambda/index.ts
+++ server-hosting/lambda/index.ts
@@ -1,28 +1,39 @@
 import { EC2Client, StartInstancesCommand } from "@aws-sdk/client-ec2";
 
-const instanceId = process.env.INSTANCE_ID
+const instanceId = process.env.INSTANCE_ID;
 const client = new EC2Client({ region: process.env.AWS_REGION });
 const command = new StartInstancesCommand({ InstanceIds: [instanceId!] });
 
 exports.handler = async function (event: any) {
-
   console.log("Attempting to start game server", instanceId);
 
-  return client.send(command)
+  return client
+    .send(command)
     .then((res) => {
       console.log(JSON.stringify(res));
       return {
         statusCode: 200,
         headers: { "Content-Type": "text/json" },
-        body: JSON.stringify({ message: "Started satisfactory server", response: JSON.stringify(res) })
-      }
+        body: JSON.stringify({
+          message: "Started satisfactory server.",
+          response: `Started satisfactory server. Metadata(for bots, don't worry about this): ${JSON.stringify(
+            res
+          )}`,
+        }),
+      };
     })
     .catch((err) => {
       console.log(JSON.stringify(err));
       return {
-        statusCode: 200,
+        statusCode: 500,
         headers: { "Content-Type": "text/json" },
-        body: JSON.stringify({ message: "Failed to start satisfactory server", response: JSON.stringify(err) })
-      }
+        body: JSON.stringify({
+          message:
+            "Failed to start satisfactory server. Tell Bots to fix his shit.",
+          response: `Failed to start satisfactory server. Tell bots to fix his shit. Error(for bots, don't worry about this): ${JSON.stringify(
+            err
+          )}`,
+        }),
+      };
     });
-}
+};
